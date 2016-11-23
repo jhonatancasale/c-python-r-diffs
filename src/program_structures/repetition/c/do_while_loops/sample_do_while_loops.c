db3f770f29821809ecf9b7e8e8d478cbb01fb531
jhonatancasale <jhonatan@jhonatancasale.com>
2016-11-23 04:34:47 -0200

    Add Samples for C repetition loops usages

---
+++ src/program_structures/repetition/c/do_while_loops/sample_do_while_loops.c
@@ -0,0 +1,27 @@
+/*
+ * Author: Jhonatan Casale (jhc)
+ *
+ * Contact : jhonatan@jhonatancasale.com
+ *         : casale.jhon@gmail.com
+ *         : https://github.com/jhonatancasale
+ *         : https://twitter.com/jhonatancasale
+ *         : http://jhonatancasale.github.io/
+ *
+ * Create date Wed 23 Nov 03:08:02 BRST 2016
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  int i = 0;
+
+  do {
+    printf ( "%d\n", i ); 
+  } while ( i++ < 10 );
+
+
+  return (EXIT_SUCCESS);
+}
