1929cd6fb4166decd5c972e5a428c32b172f36c1
jhonatancasale <jhonatan@jhonatancasale.com>
2016-11-29 07:00:03 -0200

    Remove dummy spaces

--- src/program_structures/decision/python/ternary_statement/sample_ternary_statement.py
+++ src/program_structures/decision/python/ternary_statement/sample_ternary_statement.py
@@ -1,12 +1,12 @@
 """
   Author: Jhonatan Casale (jhc)
- 
+
   Contact : jhonatan@jhonatancasale.com
           : casale.jhon@gmail.com
           : https://github.com/jhonatancasale
           : https://twitter.com/jhonatancasale
           : http://jhonatancasale.github.io/
- 
+
   Create date Sat 26 Nov 04:17:13 BRST 2016
 """
 
@@ -14,4 +14,4 @@ x = -1
 y = 1 if x > 0 else 0
 
 print(y)
-print("{}".format( "Positive" if y else "Negative" ) )
+print("{}".format("Positive" if y else "Negative"))
